a81b5ed7f44f88570b5a884a169a633f8de7937b
Youssef Restom <youssefrestom@gmail.com>
2021-06-24 23:29:38 +0000

    update doctypes

--- servicems/service_management/doctype/service_job_card/service_job_card.py
+++ servicems/service_management/doctype/service_job_card/service_job_card.py
@@ -1,8 +1,9 @@
 # Copyright (c) 2021, Aakvatech Limited and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
+
 class ServiceJobCard(Document):
-	pass
+    pass
