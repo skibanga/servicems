fe6a4074ebeadb225d9b6332e63fcea0e765d526
Youssef Restom <youssefrestom@gmail.com>
2021-06-25 17:04:49 +0000

    feat: set rows in chaild tabels in Service Job Card

--- servicems/service_management/doctype/service_job_card/service_job_card.py
+++ servicems/service_management/doctype/service_job_card/service_job_card.py
@@ -6,4 +6,27 @@ from frappe.model.document import Document
 
 
 class ServiceJobCard(Document):
-    pass
+    def validate(self):
+        self.update_tabels()
+
+    def update_tabels(self):
+        for temp in self.services:
+            if not temp.applied:
+                temp_doc = frappe.get_doc("Service Template", temp.service)
+                if temp_doc.tasks:
+                    for task in temp_doc.tasks:
+                        row = self.append("tasks", {})
+                        row.task_name = task.task_name
+                        row.template = temp_doc.name
+
+                if temp_doc.parts:
+                    for part in temp_doc.parts:
+                        row = self.append("parts", {})
+                        row.part_name = part.part_name
+                        row.item = part.item
+                        row.type = part.type
+                        row.qty = part.qty
+                        row.rate = part.rate
+                        row.is_billable = part.is_billable
+
+                temp.applied = 1
