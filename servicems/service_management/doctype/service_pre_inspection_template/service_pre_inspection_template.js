510b8575a7aeae584e7677baa4c2ffbbbd9ff30a
Aakvatech Limited <info@aakvatech.com>
2021-11-29 11:01:12 +0100

    feat: Added Pre Inspection Template for Service Job Card

---
+++ servicems/service_management/doctype/service_pre_inspection_template/service_pre_inspection_template.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Aakvatech Limited and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Service Pre Inspection Template', {
+	// refresh: function(frm) {
+
+	// }
+});
