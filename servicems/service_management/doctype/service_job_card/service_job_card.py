159bf405f61797a9b3b7d025b220236780071c03
Aakvatech Limited <info@aakvatech.com>
2022-07-11 12:52:32 +0200

    fix: taxes and charges calculation

--- servicems/service_management/doctype/service_job_card/service_job_card.py
+++ servicems/service_management/doctype/service_job_card/service_job_card.py
@@ -16,8 +16,15 @@ class ServiceJobCard(WebsiteGenerator):
         self.vaildate_complete()
 
     def before_submit(self):
-        self.create_stock_entry("before_submit")
-        self.create_invoice()
+        use_parts_entry = (
+            frappe.get_value("Company Service Management Settings", "use_parts_entry")
+            or 0
+        )
+        if use_parts_entry:
+            self.create_parts_entry("before_submit")
+        else:
+            self.create_stock_entry("before_submit")
+            self.create_invoice()
 
     def on_submit(self):
         if self.status != "Completed":
@@ -79,6 +86,55 @@ class ServiceJobCard(WebsiteGenerator):
             if not item.rate:
                 item.rate = get_item_price(item.item, price_list, self.company)
 
+    # @frappe.whitelist()
+    def create_parts_entry(self, type):
+        if self.parts and len(self.parts) > 0:
+            items = []
+            for item in self.parts:
+                if item.qty > 0:
+                    items.append(
+                        {
+                            "item_code": item.item,
+                            "qty": item.qty,
+                        }
+                    )
+
+            if len(items) == 0:
+                return
+
+            doc = frappe.get_doc(
+                dict(
+                    doctype="Service Parts Entry",
+                    posting_date=nowdate(),
+                    posting_time=nowtime(),
+                    company=self.company,
+                    service_job_card=self.name,
+                    items=items,
+                ),
+            )
+            frappe.flags.ignore_account_permission = True
+            doc.insert(ignore_permissions=True)
+            doc.submit()
+            frappe.msgprint(
+                _("Service Parts Entry Created {0}").format(doc.name), alert=True
+            )
+
+            if doc.get("name"):
+                left_parts = []
+                for row in self.parts:
+                    if row.qty > 0:
+                        new_row = self.append("supplied_parts", {})
+                        new_row.item = row.item
+                        new_row.qty = row.qty
+                        new_row.rate = row.rate
+                        new_row.is_billable = row.is_billable
+                        new_row.stock_entry = doc.name
+                    else:
+                        left_parts.append(row)
+                self.parts = left_parts
+                if type == "call":
+                    self.save()
+
     @frappe.whitelist()
     def create_stock_entry(self, type):
         if self.parts and len(self.parts) > 0:
@@ -107,7 +163,7 @@ class ServiceJobCard(WebsiteGenerator):
                     stock_entry_type="Material Transfer",
                     purpose="Material Transfer",
                     company=self.company,
-                    service_job_card = self.name,
+                    service_job_card=self.name,
                     from_warehouse=workshop.parts_warehouse,
                     to_warehouse=workshop.workshop_warehouse,
                     items=items,
@@ -141,6 +197,8 @@ class ServiceJobCard(WebsiteGenerator):
         workshop = frappe.get_doc("Service Workshop", self.workshop)
         if self.services and len(self.services) > 0:
             for item in self.services:
+                if item.is_billable:
+                    continue
                 items.append(
                     {
                         "item_code": item.item,
@@ -152,6 +210,8 @@ class ServiceJobCard(WebsiteGenerator):
                 )
         if self.supplied_parts and len(self.supplied_parts) > 0:
             for item in self.supplied_parts:
+                if not item.is_billable or item.qty == 0:
+                    continue
                 items.append(
                     {
                         "item_code": item.item,
@@ -172,11 +232,16 @@ class ServiceJobCard(WebsiteGenerator):
                     posting_date=date,
                     due_date=date,
                     update_stock=1,
-                    service_job_card = self.name,
+                    service_job_card=self.name,
                     company=self.company,
                     ignore_pricing_rule=1,
                     set_warehouse=workshop.workshop_warehouse,
                     items=items,
+                    taxes_and_charges=frappe.get_value(
+                        "Sales Taxes and Charges Template",
+                        {"company": self.company, "is_default": 1},
+                        "name",
+                    ),
                 ),
             )
             frappe.flags.ignore_account_permission = True
@@ -186,9 +251,7 @@ class ServiceJobCard(WebsiteGenerator):
             doc.calculate_taxes_and_totals()
             doc.insert(ignore_permissions=True)
             self.invoice = doc.name
-            frappe.msgprint(
-                _("Saeles Invoice Created {0}").format(doc.name), alert=True
-            )
+            frappe.msgprint(_("Sales Invoice Created {0}").format(doc.name), alert=True)
 
     def vaildate_complete(self):
         if self.status != "Completed":
@@ -228,19 +291,22 @@ def get_item_price(item_code, price_list, company):
         price = item_prices_data[0].price_list_rate or 0
     return price
 
+
 @frappe.whitelist()
 def get_selected_items(items):
     selected_items = json.loads(items)
 
     if selected_items:
-        doc = frappe.get_doc(selected_items[0]["parenttype"], selected_items[0]["parent"])
+        doc = frappe.get_doc(
+            selected_items[0]["parenttype"], selected_items[0]["parent"]
+        )
         source_doc = frappe.get_doc("Stock Entry", selected_items[0]["stock_entry"])
 
         new_doc = frappe.new_doc("Stock Entry")
         new_doc.company = source_doc.company
         new_doc.stock_entry_type = source_doc.stock_entry_type
         new_doc.purpose = source_doc.purpose
-        new_doc.posting_date = nowdate() 
+        new_doc.posting_date = nowdate()
         new_doc.posting_time = nowtime()
         new_doc.from_warehouse = source_doc.to_warehouse
         new_doc.to_warehouse = source_doc.from_warehouse
@@ -249,40 +315,49 @@ def get_selected_items(items):
 
         for item in selected_items:
             if not item.get("qty_to_return"):
-                frappe.throw(_('<h4 class="text-center" style="background-color: yellow; font-weight: bold;">\
+                frappe.throw(
+                    _(
+                        '<h4 class="text-center" style="background-color: yellow; font-weight: bold;">\
                     Can not process stock entry for empty quantity to return<h4>'
-                ))
+                    )
+                )
 
             for entry in source_doc.items:
                 if item.get("item") == entry.item_code:
-                    new_doc.append("items", {
-                        "s_warehouse": entry.t_warehouse,
-                        "t_warehouse": entry.s_warehouse,
-                        "item_code": item.get("item"),
-                        "item_name": entry.item_name,
-                        "description": entry.description,
-                        "item_group": entry.item_group,
-                        "qty": item.get("qty_to_return"),
-                        "transfer_qty": item.get("qty_to_return"),
-                        "uom": entry.uom,
-                        "stock_uom": entry.stock_uom,
-                        "conversion_factor": entry.conversion_factor,
-                        "expense_account": entry.expense_account,
-                        "basic_rate": item.get("rate"),
-                        "basic_amount": item.get("rate"),
-                        "amount": item.get("rate"),
-                        "cost_center": entry.cost_center
-                    })
-                    
+                    new_doc.append(
+                        "items",
+                        {
+                            "s_warehouse": entry.t_warehouse,
+                            "t_warehouse": entry.s_warehouse,
+                            "item_code": item.get("item"),
+                            "item_name": entry.item_name,
+                            "description": entry.description,
+                            "item_group": entry.item_group,
+                            "qty": item.get("qty_to_return"),
+                            "transfer_qty": item.get("qty_to_return"),
+                            "uom": entry.uom,
+                            "stock_uom": entry.stock_uom,
+                            "conversion_factor": entry.conversion_factor,
+                            "expense_account": entry.expense_account,
+                            "basic_rate": item.get("rate"),
+                            "basic_amount": item.get("rate"),
+                            "amount": item.get("rate"),
+                            "cost_center": entry.cost_center,
+                        },
+                    )
+
         new_doc.save(ignore_permissions=True)
         new_doc.submit()
         if new_doc.get("name"):
             updated_supplied_parts(doc, selected_items, new_doc.get("name"))
-            frappe.msgprint("Stock Entry: {0} Created Successfully".format(
-                frappe.bold(new_doc.name)
-            ))
+            frappe.msgprint(
+                "Stock Entry: {0} Created Successfully".format(
+                    frappe.bold(new_doc.name)
+                )
+            )
         else:
             frappe.throw("Stock Entry was not created, try again")
+
 
 def updated_supplied_parts(doc, selected_items, name):
     for row in doc.supplied_parts:
