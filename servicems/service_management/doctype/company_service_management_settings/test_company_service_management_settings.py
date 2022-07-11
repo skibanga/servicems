ae4a34ec6f727d61197712fdd42e7f40f0491d3d
Aakvatech Limited <info@aakvatech.com>
2022-07-11 19:01:35 +0200

    feat: Service related doctypes

---
+++ servicems/service_management/doctype/company_service_management_settings/test_company_service_management_settings.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, Aakvatech Limited and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestCompanyServiceManagementSettings(unittest.TestCase):
+	pass
---
+++ servicems/service_management/doctype/service_parts_entry/service_parts_entry.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Aakvatech Limited and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Service Parts Entry', {
+	// refresh: function(frm) {
+
+	// }
+});
